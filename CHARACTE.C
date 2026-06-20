d41006fa050515c299ac7c2ba1a87aad5cd5f691
Shristhy1808 <sehgalshristhy@gmail.com>
2026-06-20 20:36:04 +0530

    Add files via upload

---
+++ CHARACTE.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	 char name[20];
+	 clrscr();
+	 scanf("%s",name);
+	 gets(name);
+	 printf("your scanned value is %s",name);
+
+	 getch();
+}
+
+
+
+
+
+
+
+
+
+
